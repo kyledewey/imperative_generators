e1b72c20b2a90866fd5680b64c82931bc3611197
Kyle Dewey <kyledewey@cs.ucsb.edu>
2017-03-27 13:50:56 -0700

    added another example

--- main.pl
+++ main.pl
@@ -11,23 +11,35 @@ use strict;
 
 # BEGIN MAIN CODE
 
-# We want the words 'true' and 'false', in an array
-my @words = ('true', 'false');
+sub printArrayOfWords($$@) {
+    my ($min, $max, @words) = @_;
+    
+    # A constructor for iterators over arrays.  Simply creates
+    # a new array iterator when called.  'sub' allows for the
+    # creation of named and anonymous functions
+    my $constructor = sub {
+        return new ArrayIterator(@words);
+    };
 
-# A constructor for iterators over arrays.  Simply creates
-# a new array iterator when called.  'sub' allows for the
-# creation of named and anonymous functions
-my $constructor = sub {
-    return new ArrayIterator(@words);
-};
+    # Creates lists of length `$min` to `$max`, where the contents are
+    # populated by the iterator returned from calling
+    # '$constructor'
+    my $listIterator = new ListIterator($min, $max, $constructor);
+    $listIterator->foreachElement(
+        sub {
+            my $arrRef = shift(); # the parameter (a list)
+            print '[' . join(', ', @$arrRef) . "]\n";
+        });
+    $listIterator->reset(); # not necessary, but good practice
+}
 
-# Creates lists of length 0 to 2, where the contents are
-# populated by the iterator returned from calling
-# '$constructor'
-my $listIterator = new ListIterator(0, 2, $constructor);
-$listIterator->foreachElement(
-    sub {
-        my $arrRef = shift(); # the parameter (a list)
-        print '[' . join(', ', @$arrRef) . "]\n";
-    });
-$listIterator->reset(); # not necessary, but good practice
+# booleans
+print "BOOLEANS:\n";
+printArrayOfWords(0, 2, 'true', 'false');
+
+print "\n";
+
+# arbitrary words
+print "ARBITRARY WORDS:\n";
+printArrayOfWords(0, 3, 'foo', 'bar', 'baz');
+
