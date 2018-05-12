ee1485c2ad8cf5c14354d91dca44395e0d2c39d7
Kyle Dewey <kyledewey@cs.ucsb.edu>
2018-05-12 00:46:03 -0700

    broken and

---
+++ scala_nondeterminism/generator.scala
@@ -0,0 +1,70 @@
+trait Generator[+A] {
+  def next(): Option[A]
+  def reset(): Unit
+
+  def and[B](f: A => Generator[B]): Generator[B] = {
+    next() match {
+      case Some(a) => f(a)
+      case None => new EmptyGenerator
+    }
+  }
+
+  def or[B >: A](otherRaw: => Generator[B]): Generator[B] = {
+    lazy val other = otherRaw
+    val self = this
+    new Generator[B] {
+      def next(): Option[B] = {
+        self.next() match {
+          case s@Some(_) => s
+          case None => other.next()
+        }
+      }
+      def reset() {
+        self.reset()
+        other.reset()
+      }
+    }
+  }
+
+  def flatMap[B](f: A => Generator[B]): Generator[B] = and(f)
+  def map[B](f: A => B): Generator[B] = {
+    val self = this
+    new Generator[B] {
+      def next(): Option[B] = {
+        self.next().map(f)
+      }
+      def reset() {
+        self.reset()
+      }
+    }
+  }
+}
+
+class EmptyGenerator[A] extends Generator[A] {
+  def next(): Option[A] = None
+  def reset() {}
+}
+
+class SingletonGenerator[A](val value: A) extends Generator[A] {
+  private var gotten = false
+  def next(): Option[A] = {
+    if (gotten) {
+      None
+    } else {
+      gotten = true
+      Some(value)
+    }
+  }
+
+  def reset() {
+    gotten = false
+  }
+}
+
+object Generator {
+  import scala.language.implicitConversions
+
+  implicit def anyToGenerator[A](a: A): Generator[A] = {
+    new SingletonGenerator(a)
+  }
+}
---
+++ scala_nondeterminism/grammar.scala
@@ -0,0 +1,80 @@
+sealed trait Exp {
+  def asString(): String
+}
+case class IntLiteral(i: Int) extends Exp {
+  def asString(): String = i.toString
+}
+case class Binop(e1: Exp, op: Op, e2: Exp) extends Exp {
+  def asString(): String = {
+    Seq(e1.asString, op.asString, e2.asString).mkString(" ")
+  }
+}
+case class Paren(e: Exp) extends Exp {
+  def asString(): String = {
+    "(" + e.asString + ")"
+  }
+}
+
+sealed trait Op {
+  def asString(): String
+}
+case object Plus extends Op {
+  def asString(): String = "+"
+}
+case object Minus extends Op {
+  def asString(): String = "-"
+}
+case object Mult extends Op {
+  def asString(): String = "*"
+}
+case object Div extends Op {
+  def asString(): String = "/"
+}
+
+object Exp {
+  import Generator._
+
+  def literal(): Generator[IntLiteral] = {
+    IntLiteral(0).or(
+      IntLiteral(1).or(
+        IntLiteral(2)))
+  }
+
+  def op(): Generator[Op] = {
+    (Plus: Op).or(
+      (Minus: Op).or(
+        (Mult: Op).or(
+          Div: Op)))
+  }
+
+  def withDecBound[A](depth: Int)(f: Int => Generator[A]): Generator[A] = {
+    if (depth > 0) {
+      f(depth - 1)
+    } else {
+      new EmptyGenerator
+    }
+  }
+
+  def binop(depth: Int): Generator[Binop] = {
+    withDecBound(depth)(depth =>
+      for {
+        e1 <- exp(depth)
+        o <- op
+        e2 <- exp(depth)
+      } yield Binop(e1, o, e2))
+  }
+
+  def paren(depth: Int): Generator[Paren] = {
+    withDecBound(depth)(depth =>
+      for {
+        e <- exp(depth)
+      } yield Paren(e))
+  }
+
+  def exp(depth: Int): Generator[Exp] = {
+    (literal: Generator[Exp]).or(
+      (binop(depth): Generator[Exp]).or(
+        (paren(depth): Generator[Exp])))
+  }
+}
+
